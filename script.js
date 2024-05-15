0ca79a454367de7009f7c2bf2fb84c95a92073de
Vineeth Ravoori <145092290+persistent-hustler@users.noreply.github.com>
2024-05-15 13:37:22 +0530

    Accept user input for cells per side

--- script.js
+++ script.js
@@ -1,17 +1,33 @@
 let container = document.querySelector('#container');
 let numberOfCells = 16;
+generateGrid();
 
-for(i = 0; i < numberOfCells; i++) {
-    let line = document.createElement('div');
-    line.setAttribute('style', 'display: flex;');
+let btn = document.querySelector('button');
+btn.addEventListener('click', function () {
+    let userInput = +prompt('Pick cells per side (1 to 100)');
 
-    for(j = 0; j < numberOfCells; j++) {
-        let cell = document.createElement('div');
-        let cellWidth = `${600/numberOfCells}px`;
-
-        cell.setAttribute('style', `width: ${cellWidth}; height: ${cellWidth}; border: 1px solid black;`);
-        cell.addEventListener('mouseenter', () => cell.setAttribute('class', 'writtenCell'));
-        line.appendChild(cell);
+    if(userInput >= 1 && userInput <= 100) {
+        numberOfCells = userInput;
+    } else {
+        alert('You can pick between 1 and 100.');
     }
-    container.appendChild(line);
+    generateGrid();
+});
+
+function generateGrid() {
+    container.innerHTML = '';
+    for(i = 0; i < numberOfCells; i++) {
+        let line = document.createElement('div');
+        line.setAttribute('style', 'display: flex;');
+    
+        for(j = 0; j < numberOfCells; j++) {
+            let cell = document.createElement('div');
+            let cellWidth = `${600/numberOfCells}px`;
+    
+            cell.setAttribute('style', `width: ${cellWidth}; height: ${cellWidth}; border: 1px solid black;`);
+            cell.addEventListener('mouseenter', () => cell.setAttribute('class', 'writtenCell'));
+            line.appendChild(cell);
+        }
+        container.appendChild(line);
+    }
 }
